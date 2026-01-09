02e1fb57aac9c783dca3e2828d0fe1a2fab559e0
colinarelliott <colinarelliott@gmail.com>
2026-01-09 13:01:31 -0500

    fixed the chest textures

--- src/main/java/rehdpanda/utilitygolems/client/GolemChestBlockEntityRenderer.java
+++ src/main/java/rehdpanda/utilitygolems/client/GolemChestBlockEntityRenderer.java
@@ -32,7 +32,7 @@ public class GolemChestBlockEntityRenderer implements BlockEntityRenderer<GolemC
 
     static {
         for (GolemType type : GolemType.values()) {
-            TEXTURES.put(type, new SpriteIdentifier(TexturedRenderLayers.CHEST_ATLAS_TEXTURE, Identifier.of("minecraft", "entity/chest/utility-golems/" + type.getName() + "_chest")));
+            TEXTURES.put(type, new SpriteIdentifier(TexturedRenderLayers.CHEST_ATLAS_TEXTURE, Identifier.of(UGInit.MOD_ID, "entity/chest/" + type.getName() + "_chest")));
         }
     }
 
